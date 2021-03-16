91ec349a49f0000633ab2e9957769b34f92967af
HCS Learning <learning@hardcybersoft.cl>
2021-03-16 01:22:56 -0300

    configuracion para CommonJS y ESM sin usar Webpack, solo usando Typescript

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,13 @@
-export default function getOffset(){
+import { Producto } from "../type/index";
+
+export function getOffset():number {
     return 1
+}
+
+export function getPrecioFinal(p:Producto):number {
+    if( p.precio_oferta != null && p.precio_oferta > 0) {
+        return p.precio_oferta
+    } else {
+        return p.precio_normal
+    }
 }
